3f721ffdd5910d5ff791c331eccf5a1ea0b97662
Chris Patterson <chris@phatboyg.com>
2021-03-15 17:46:46 -0500

    Removed noise

--- src/messageContext.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MessageContext = void 0;
-var MessageContext = /** @class */ (function () {
-    function MessageContext() {
-    }
-    return MessageContext;
-}());
-exports.MessageContext = MessageContext;
